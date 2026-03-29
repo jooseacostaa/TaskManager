347f4c7f9301977a519d99fa11a5073e8a15698d
jooseacostaa <joseacostanaranjo@hotmail.com>
2026-03-29 21:00:29 +0100

    Building Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,45 @@
-void main() {
-    
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void mostrarMenu() {
+
+        System.out.println("--- GESTOR DE TAREAS ---" + "\n" +
+                "Pulse una tecla según la opción que quiera realizar" + "\n" +
+                "1. Añadir tarea" + "\n" +
+                "2. Ver tareas" + "\n" +
+                "3. Completar tareas" + "\n" +
+                "4 Eliminar tareas" + "\n" +
+                "5. Salir");
+
+    }
+
+    public static int leerOpcion() {
+
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        opcion = sc.nextInt();
+        return opcion;
+
+    }
+
+    public static boolean procesarOpcion() {
+
+        if (leerOpcion() != 1
+                || leerOpcion() != 2
+                || leerOpcion() != 3
+                || leerOpcion() != 4
+                || leerOpcion() != 5) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
 }
