03c587242dc30c5e2b054079456574b16466cc00
jooseacostaa <joseacostanaranjo@hotmail.com>
2026-04-04 18:32:08 +0100

    Upgrading Tarea class to use files

--- src/Tarea.java
+++ src/Tarea.java
@@ -8,12 +8,29 @@ public class Tarea {
         this.completada = false;
     }
 
+    public Tarea(String titulo, boolean completada) {
+        this.titulo = titulo;
+        this.completada = completada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
     public void marcarComoCompletada() {
         this.completada = true;
     }
 
+    public String toFileString() {
+        return titulo + ";" + completada;
+    }
+
     @Override
     public String toString() {
-        return (completada ? "[ X ] " : "[ ] " + titulo);
+        return (completada ? "[X] " : "[ ] ") + titulo;
     }
 }
